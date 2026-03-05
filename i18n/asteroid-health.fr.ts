9f98723893d3425775975870a68cd567e8d5f65c
Nicolas Chauvat <nicolas.chauvat@logilab.fr>
2026-03-06 00:29:11 +0100

    Add French translation

---
+++ i18n/asteroid-health.fr.ts
@@ -0,0 +1,101 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="fr">
+<context>
+    <name></name>
+    <message id="id-app-launcher-name">
+        <source>Health</source>
+        <translation>Santé</translation>
+    </message>
+    <message id="id-overview">
+        <source>Overview</source>
+        <translation>Résumé</translation>
+    </message>
+    <message id="id-settings">
+        <source>Settings</source>
+        <translation>Paramètres</translation>
+    </message>
+    <message id="id-ui-settings">
+        <source>UI settings</source>
+        <translation>Paramètres IHM</translation>
+    </message>
+    <message id="id-logger-settings">
+        <source>Logger settings</source>
+        <translation>Paramètres log</translation>
+    </message>
+    <message id="id-ui-settings-title">
+        <source>UI Settings</source>
+        <translation>Paramètres IHM</translation>
+    </message>
+    <message id="id-show-step-count-preview">
+        <source>Show step count preview</source>
+        <translation>Affiche résumé décompte pas</translation>
+    </message>
+    <message id="id-show-heartrate-preview">
+        <source>Show heartrate preview</source>
+        <translation>Affiche résumé fréquence cardiaque</translation>
+    </message>
+    <message id="id-show-weight-preview">
+        <source>Show weight preview</source>
+        <translation>Affiche résumé poids</translation>
+    </message>
+    <message id="id-logger-settings-title">
+        <source>Logger Settings</source>
+        <translation>Paramètres log</translation>
+    </message>
+    <message id="id-log-step-count">
+        <source>Log step count</source>
+        <translation>Comptage des pas</translation>
+    </message>
+    <message id="id-enable-steps-goal">
+        <source>Enable steps goal</source>
+        <translation>Objectif de pas</translation>
+    </message>
+    <message id="id-log-heart-rate">
+        <source>Log heart rate</source>
+        <translation>Suivi fréquence cardiaque</translation>
+    </message>
+    <message id="id-steps-walked-today">
+        <source>You've walked %1 steps today, keep it up!</source>
+        <extracomment>%1 is the number of steps</extracomment>
+        <translation>Vous avez fait %1 pas aujourd'hui, continuez !</translation>
+    </message>
+    <message id="id-no-steps-today">
+        <source>You haven't yet logged any steps today</source>
+        <translation>Aucun pas enregistré aujourd'hui</translation>
+    </message>
+    <message id="id-steps">
+        <source>Steps</source>
+        <translation>Pas</translation>
+    </message>
+    <message id="id-steps-walked-on-day">
+        <source>You walked %1 steps on this day</source>
+        <extracomment>%1 is the number of steps</extracomment>
+        <translation>Vous avez fait %1 pas aujourd'hui</translation>
+    </message>
+    <message id="id-3-weeks">
+        <source>3 weeks</source>
+        <translation>3 semaines</translation>
+    </message>
+    <message id="id-week">
+        <source>week</source>
+        <translation>semaine</translation>
+    </message>
+    <message id="id-day">
+        <source>day</source>
+        <translation>jour</translation>
+    </message>
+    <message id="id-heartrate">
+        <source>Heartrate</source>
+        <translation>Cardiofréquence</translation>
+    </message>
+    <message id="id-weight">
+        <source>Weight</source>
+        <translation>Poids</translation>
+    </message>
+    <message id="id-add-record">
+        <source>Add record</source>
+        <translation>Ajoute record</translation>
+    </message>
+</context>
+</TS>
