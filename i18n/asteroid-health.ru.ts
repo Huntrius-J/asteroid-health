c06b460bc8e733dd7924b11cb070d1cdf0d5da30
huntrius <huntrhomka@gmail.com>
2026-03-16 12:44:07 +0700

    Add Russian translation

---
+++ i18n/asteroid-health.ru.ts
@@ -0,0 +1,101 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ru">
+<context>
+    <name></name>
+    <message id="id-app-launcher-name">
+        <source>Health</source>
+        <translation>Здоровье</translation>
+    </message>
+    <message id="id-overview">
+        <source>Overview</source>
+        <translation>Обзор</translation>
+    </message>
+    <message id="id-settings">
+        <source>Settings</source>
+        <translation>Настройки</translation>
+    </message>
+    <message id="id-ui-settings">
+        <source>UI settings</source>
+        <translation>Настройки UI</translation>
+    </message>
+    <message id="id-logger-settings">
+        <source>Logger settings</source>
+        <translation>Настройки журнала</translation>
+    </message>
+    <message id="id-ui-settings-title">
+        <source>UI Settings</source>
+        <translation>Настройки UI</translation>
+    </message>
+    <message id="id-show-step-count-preview">
+        <source>Show step count preview</source>
+        <translation>Показывать превью счетчика шагов</translation>
+    </message>
+    <message id="id-show-heartrate-preview">
+        <source>Show heartrate preview</source>
+        <translation>Показывать превью измерений пульса</translation>
+    </message>
+    <message id="id-show-weight-preview">
+        <source>Show weight preview</source>
+        <translation>Показывать превью измерений веса</translation>
+    </message>
+    <message id="id-logger-settings-title">
+        <source>Logger Settings</source>
+        <translation>Настройки журнала</translation>
+    </message>
+    <message id="id-log-step-count">
+        <source>Log step count</source>
+        <translation>Записывать шаги</translation>
+    </message>
+    <message id="id-enable-steps-goal">
+        <source>Enable steps goal</source>
+        <translation>Цель достижения шагов</translation>
+    </message>
+    <message id="id-log-heart-rate">
+        <source>Log heart rate</source>
+        <translation>Записывать пульс</translation>
+    </message>
+    <message id="id-steps-walked-today">
+        <source>You've walked %1 steps today, keep it up!</source>
+        <extracomment>%1 is the number of steps</extracomment>
+        <translation>Сегодня вы прошли %1 шагов, продолжайте в том же духе!</translation>
+    </message>
+    <message id="id-no-steps-today">
+        <source>You haven't yet logged any steps today</source>
+        <translation>Сегодня вы не прошли ни одного шага</translation>
+    </message>
+    <message id="id-steps">
+        <source>Steps</source>
+        <translation>Шаги</translation>
+    </message>
+    <message id="id-steps-walked-on-day">
+        <source>You walked %1 steps on this day</source>
+        <extracomment>%1 is the number of steps</extracomment>
+        <translation>В этот день вы прошли %1 шагов</translation>
+    </message>
+    <message id="id-3-weeks">
+        <source>3 weeks</source>
+        <translation>3 недели</translation>
+    </message>
+    <message id="id-week">
+        <source>week</source>
+        <translation>неделя</translation>
+    </message>
+    <message id="id-day">
+        <source>day</source>
+        <translation>день</translation>
+    </message>
+    <message id="id-heartrate">
+        <source>Heartrate</source>
+        <translation>Пульс</translation>
+    </message>
+    <message id="id-weight">
+        <source>Weight</source>
+        <translation>Вес</translation>
+    </message>
+    <message id="id-add-record">
+        <source>Add record</source>
+        <translation>Добавить запись</translation>
+    </message>
+</context>
+</TS>
